a1afb1153b739d31807cae2b687c92a1cf0827a5
tom_glb <tmo_glb@163.com>
2022-04-25 21:36:48 +0800

    http_single_paqu

--- src/http/http_client/main.go
+++ src/http/http_client/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	resp, err := http.Get("http://127.0.0.1:8000")
+	if err != nil {
+		fmt.Println("http.Get err ", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("header = ", resp.Header)
+	fmt.Println("status = ", resp.Status)
+	fmt.Println("statuscode = ", resp.StatusCode)
+	fmt.Printf("body type = %T\n", resp.Body)
+
+	buf := make([]byte, 1024)
+	var str string
+	for {
+		n, err2 := resp.Body.Read(buf)
+		if err2 != nil {
+			fmt.Println("resp.Body.Read ", err2)
+		}
+		if n == 0 {
+			fmt.Println("End of Read")
+			break
+		}
+		str += string(buf[:n])
+	}
+
+	fmt.Println("str ", str)
 
 }
