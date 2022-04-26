65834fee9c2943116ce0f0ca0ae2bf91964692ef
tom_glb <tmo_glb@163.com>
2022-04-26 21:41:27 +0800

    multi_routine

--- src/http/http_client_paqu/main.go
+++ src/http/http_client_paqu/main.go
@@ -20,11 +20,11 @@ func httpGet(url string) (content string, err error) {
 	for {
 		n, err := resp.Body.Read(buf)
 		if err != nil {
-			fmt.Println("resp.Body.Read err = ", err)
+			//fmt.Println("resp.Body.Read err = ", err)
 			break
 		}
 		if n == 0 {
-			fmt.Println("读完了已经")
+			//fmt.Println("读完了已经")
 			break
 		}
 		content += string(buf)
@@ -32,32 +32,42 @@ func httpGet(url string) (content string, err error) {
 	return
 }
 
-func doWork(start_page, end_page int, AHU_tiba string) {
+func spiderPage(i, num int, AHU_tieba string, page chan int) {
+	url := AHU_tieba + strconv.Itoa(i*num)
+	fmt.Printf("正在爬取第%d个网页 : %s\n", i, url)
+	content, err := httpGet(url)
+	if err != nil {
+		fmt.Println("httpGet err = ", err)
+		return
+	}
+	file_name := strconv.Itoa(i) + ".html"
+	f, err2 := os.Create(file_name)
+	if err2 != nil {
+		fmt.Println("os.Create err2 = ", err2)
+		return
+	}
+	f.Write([]byte(content))
+	f.Close()
+	page <- i
+}
+
+func doWork(start_page, end_page int, AHU_tieba string) {
 	num := 50
-	var url string
-	for i := start_page - 1; i <= end_page; i++ {
-		url = AHU_tiba + strconv.Itoa(i*num)
-		content, err := httpGet(url)
-		if err != nil {
-			fmt.Println("httpGet err = ", err)
-			continue
-		}
-		file_name := strconv.Itoa(i) + ".html"
-		f, err2 := os.Create(file_name)
-		if err2 != nil {
-			fmt.Println("os.Create err2 = ", err2)
-			continue
-		}
-		f.Write([]byte(content))
+	page := make(chan int)
+	for i := start_page - 1; i <= end_page-1; i++ {
+		go spiderPage(i, num, AHU_tieba, page)
+	}
+	for i := start_page - 1; i <= end_page-1; i++ {
+		fmt.Printf("第%d个页面爬取完毕\n", <-page)
 	}
 }
 
 func main() {
-	AHU_tiba := "https://tieba.baidu.com/f?kw=%E5%AE%89%E5%BE%BD%E5%A4%A7%E5%AD%A6&ie=utf-8&pn="
+	AHU_tieba := "https://tieba.baidu.com/f?kw=%E5%AE%89%E5%BE%BD%E5%A4%A7%E5%AD%A6&ie=utf-8&pn="
 	fmt.Println("plz input start and end page")
 	var start_page, end_page int
 	fmt.Scanf("%d %d", &start_page, &end_page)
-	fmt.Printf("开始 : %d 结束 : %d", start_page, end_page)
-	doWork(start_page, end_page, AHU_tiba)
+	fmt.Printf("开始 : %d 结束 : %d\n", start_page, end_page)
+	doWork(start_page, end_page, AHU_tieba)
 
 }
